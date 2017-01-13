defd787467b79068b064ae1b1ccc5eacd0ca746d
Varje Kass <varjekass@gmail.com>
2017-01-13 12:08:42 +0200

    CSS styles and adding divs

--- index.php
+++ index.php
@@ -9,21 +9,26 @@
 //create and template object
 define('CLASSES_DIR', 'classes/');
 define('TMPL_DIR', 'tmpl/');
+define('STYLE_DIR', 'CSS/');
 require_once CLASSES_DIR.'template.php';
 //and use it
 //create an empty template object
-$tmpl = new template('main.html');
+$tmpl = new template('main');
 // add pairs of template element names and real values
+$tmpl->set('style', STYLE_DIR.'main'.'.css');
+$tmpl->set('header', 'minu pealkiri');
 $tmpl->set('menu', 'minu menüü');
 $tmpl->set('nav_bar', 'minu navigatsioon');
 $tmpl->set('lang_bar', 'minu keeleriba');
 $tmpl->set('content', 'minu sisu');
 
+/*
 // set up the file name for template
 //control the content of template object
 echo '<pre>';
 print_r($tmpl);
 echo '</pre>';
+*/
 
 echo $tmpl->parse();
 
