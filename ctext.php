bb1fc33a49b4539abae6c0448493dc98624573fb
Varje Kass <varjekass@gmail.com>
2017-01-13 12:09:20 +0200

    inital version

---
+++ ctext.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Elitebook 1020
+ * Date: 13-Jan-17
+ * Time: 12:04
+ */
+// import text class variables and methods
+require_once 'text.php';
+class ctext extends text
+{//begin of class
+
+}//end of class
