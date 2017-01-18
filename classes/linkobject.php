0574f0543d7b5c67d1498b23ed5ad7a9dbce9de3
Varje Kass <varjekass@gmail.com>
2017-01-18 16:19:10 +0200

    addtolink

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -21,6 +21,12 @@ class linkobject extends http
     function __construct(){
         parent::__construct(); //import http class construct
         $this->baseUrl = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    }//construct
+    //create http data pairs and merge them
+    //pair is element_name = element_value, for example name=admin
+    //name1=value1&name2=value2
+    function addToLink($link, $name, $val){
+        $link = $name.$this->eq.$val;
     }
 }//class end
 ?>
