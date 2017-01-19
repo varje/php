4f0fe13dbd1e0791623326a0dcfe7326aa1b4a23
Varje Kass <varjekass@gmail.com>
2017-01-19 11:13:38 +0200

    menu test

--- menu.php
+++ menu.php
@@ -19,14 +19,15 @@ $item->set('name'. 'Teine leht');
 $link= $http->getLink(array('act'=>'first'));
 $item->set('link', $link);
 //add item to menu
-$menu->set('items', $item->parse());
+$menu->add('items', $item->parse());
 //output objects
 //menu
-echo '<pre>';
-print_r($menu);
-echo '</pre>';
-//item
-echo '<pre>';
-print_r($item);
-echo '</pre>';
+//echo '<pre>';
+//print_r($menu);
+//echo '</pre>';
+////item
+//echo '<pre>';
+//print_r($item);
+//echo '</pre>';
+echo $menu->parse();
 ?>
