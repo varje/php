16077247e4f075a29c8ae6df8b2623d1248fb790
Varje Kass <varjekass@gmail.com>
2017-01-20 12:33:14 +0200

    act.php default act

--- act.php
+++ act.php
@@ -10,5 +10,9 @@ $act = $http->get('act'); //act value from url
 $fn = ACTS_DIR.str_replace('.', '/', $act).'.php';
 if(file_exists($fn) and is_file($fn) and is_readable($fn)) {
     require_once $fn;
+} else {
+    $fn = ACTS_DIR.DEFAULT_ACT.'.php';
+    $http->set('act', DEFAULT_ACT);
+    require_once $fn;
 }
 ?>
