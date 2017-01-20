532540e50f0904b678196edf595d020de13f31b5
Varje Kass <varjekass@gmail.com>
2017-01-20 09:41:42 +0200

    database with real values (db_conf file not commited, private)

--- conf.php
+++ conf.php
@@ -14,9 +14,11 @@ require_once CLASSES_DIR.'template.php'; // import template class file
 require_once CLASSES_DIR.'http.php'; // import http class file
 require_once CLASSES_DIR.'linkobject.php'; // import linkobject file
 require_once CLASSES_DIR.'mysql.php';//import database class file
+//import database configuration
+require_once 'db_conf.php';
 // objects
 // create linkobject object, because it extends http object
 $http = new linkobject();
 //create database class object
-$db = new mysql('localhost', 'user', 'parool', 'andmebaas');
+$db = new mysql(DBHOST, DBUSER, DBPASS, DBNAME);
 ?>
