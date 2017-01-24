c70ee23d93e5424699ca60cb4393dc8c6b036a25
Varje Kass <varjekass@gmail.com>
2017-01-24 15:42:01 +0200

    Session tutorialrepublic examples

--- sessionstest.php
+++ sessionstest.php
@@ -1,39 +1,55 @@
 <?php
-// Start the session
+/*// Start the session
 session_start();
-?>
+*/?>
 <!DOCTYPE html>
 <html>
 <body>
-
+<!--
 <?php
-// Set session variables
+/*// Set session variables
 $_SESSION["favcolor"] = "green";
 $_SESSION["favanimal"] = "cat";
 echo "Session variables are set.";
-?>
+*/?>
 
 <?php
-// Echo session variables that were set on previous page
+/*// Echo session variables that were set on previous page
 echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
 echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
-?>
+*/?>
 
 <?php
-print_r($_SESSION);
-?>
+/*print_r($_SESSION);
+*/?>
 
 <?php
-// to change a session variable, just overwrite it
+/*// to change a session variable, just overwrite it
 $_SESSION["favcolor"] = "yellow";
 print_r($_SESSION);
-?>
+*/?>
 
-<?php
-// remove all session variables
+--><?php
+/*// remove all session variables
 session_unset();
 
 // destroy the session
+session_destroy();
+*/?>
+<?php
+// Starting session
+session_start();
+
+// Storing session data
+$_SESSION["firstname"] = "Peter";
+$_SESSION["lastname"] = "Parker";
+echo 'Hi, ' . $_SESSION["firstname"] . ' ' . $_SESSION["lastname"];
+// Removing session data
+if(isset($_SESSION["lastname"])){
+    unset($_SESSION["lastname"]);
+}
+echo 'Hi, ' . $_SESSION["firstname"] . ' ' . $_SESSION["lastname"];
+// Destroying session
 session_destroy();
 ?>
 
