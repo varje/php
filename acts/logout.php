c183256a100bcee23624503aafac8064a70e7a43
Varje Kass <varjekass@gmail.com>
2017-02-06 17:05:56 +0200

    logout

---
+++ acts/logout.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Elitebook 1020
+ * Date: 06-Feb-17
+ * Time: 17:05
+ */
+$sess->deleteSession();
+$http->redirect();
+?>
--- menu.php
+++ menu.php
@@ -7,21 +7,35 @@
  */
 // create menu and item objects
 $menu = new template('menu.menu');
+$menu->set('items', false); //only signed in user sees menu
 $item = new template('menu.item');
 //main menu content query
-$sql = 'SELECT content_id, title FROM content WHERE parent_id=0 AND show_in_menu=1 ORDER BY sort ASC';
+$sql = 'SELECT content_id, title FROM content WHERE parent_id=0 AND show_in_menu=1';
+//show only to admin
+if(ROLE_ID != ROLE_ADMIN)
+{
+    $sql .= ' AND is_hidden=0';
+}
+$sql .= ' ORDER BY sort ASC';
 // get menu data from database
 $res = $db->getArray($sql);
 //create menu items from query result
 if($res != false) {
     foreach ($res as $page) {
-        $item->set('name', $page['title']);
         $link = $http->getLink(array('page_id'=>$page['content_id']));
         $item->set('link',$link);
+        $item->set('name', $page['title']);
         // add item to menu
         $menu->add('items', $item->parse());
     }
 }
-
+//option to log out
+if(USER_ID != ROLE_NONE)
+{
+    $link = $http->getLink(array('act' => 'logout'));
+    $item->set('link', $link);
+    $item->set('name', 'Logi v&auml;lja');
+    $menu->add('items', $item->parse());
+}
 $tmpl->set('menu', $menu->parse());
 ?>
